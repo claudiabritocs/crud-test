fa7f567aef08967dd75e592c6a458c04dded4902
claudiabritocs <claudiabrito.pro@gmail.com>
2022-02-18 10:14:07 -0300

    Fix DB seed

--- crud-test/database/seeders/CalendarioTableSeeder.php
+++ crud-test/database/seeders/CalendarioTableSeeder.php
@@ -1,16 +1,18 @@
-<?php 
- 
-namespace Database\Seeders; 
- 
-use Illuminate\Database\Seeder; 
-use Illuminate\Support\Facades\DB; 
- 
-class CalendarioTableSeeder extends Seeder 
-{ 
-    public function run() 
-    { 
-        DB::table('calendario')->insert([ 
-            'dia'     => '2022-02-17', 
-        ]); 
-    } 
+<?php  
+  
+namespace Database\Seeders;  
+  
+use Illuminate\Database\Seeder;  
+use Illuminate\Support\Facades\DB;  
+  
+class CalendarioTableSeeder extends Seeder  
+{  
+    public function run()  
+    {  
+        DB::table('calendario')->insert([  
+            'dia'     => '2022-02-17',  
+            'created_at'     => '2022-02-17 10:11:48',  
+            'updated_at'     => '2022-02-17 10:11:48',  
+        ]);  
+    }  
 }
